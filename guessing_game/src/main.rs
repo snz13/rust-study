5d8067b69e48ed55778ad3c57b797e8d754a9957
Alexander <sanzes13@gmail.com>
2022-09-23 23:33:07 +0300

    Finish game main loop.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,18 +1,38 @@
 use std::io;    // Элемент стандартной библиотеки, не входящий в "прелюдию"
+use std::cmp::Ordering;
+use rand::Rng;
 
 // Точка входа в программу
 fn main() {
     println!("Guess the number");
 
-    println!("Please input your guess");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    // Создаём изменяемую переменную связанную с экземпляром String
-    let mut guess = String::new();
+    loop {
 
-    // Обрабатываем пользовательский ввод
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line"); // Обработка сбоя в программе
+        // Макрос для вывода в stdout
+        println!("Please input your guess");
 
-    println!("You guessed: {guess}");
+        // Создаём изменяемую переменную связанную с экземпляром String
+        let mut guess = String::new();
+
+        // Обрабатываем пользовательский ввод
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line"); // Обработка сбоя в программе
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {guess}");
+
+        // Сравниваем переменную с помощью функции cmp
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Yot win!!");
+                break;
+            }
+        }
+    }
 }
